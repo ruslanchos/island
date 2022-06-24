f70dfab55f97984142fb687ca96dfda4b9365e13
Ruslan A.Bityutskih <r.bituckih@gmail.com>
2022-06-24 18:48:52 +0300

    010 Sleep and currect Random

--- src/main/java/com/javarush/island/bityutskih/services/Random.java
+++ src/main/java/com/javarush/island/bityutskih/services/Random.java
@@ -6,4 +6,12 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Random {
     private Random() {
     }
+
+    public static int getRandom(int num) {
+        return ThreadLocalRandom.current().nextInt(num);
+    }
+    public static int getRandom(int from, int num) {
+        return ThreadLocalRandom.current().nextInt(from, num);
+    }
+
 }
---
+++ src/main/java/com/javarush/island/bityutskih/services/Sleep.java
@@ -0,0 +1,14 @@
+package com.javarush.island.bityutskih.services;
+
+public class Sleep {
+    private Sleep() {
+    }
+
+    public static void sleep(int timeout) {
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
